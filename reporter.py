e9dd06f7ff3400ecd2db4f5ccd6b55a5afe750ab
sma299 <59669463+sma299@users.noreply.github.com>
2020-02-05 17:52:01 -0500

    read in from csv file

--- reporter.py
+++ reporter.py
@@ -10,4 +10,19 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+print("READING GRADEBOOK CSV FILE...")
+
+#creating a filepath!
+import pandas
+
+
+#if CSV file in same directory as this Python script:
+csv_filepath = "gradebook.csv"
+
+#csv_filepath = "/path/to/jeter_stats.csv"
+grades = pandas.read_csv(csv_filepath)
+print("GRADES:", type(grades))
+
+print(grades.head())
+
+
